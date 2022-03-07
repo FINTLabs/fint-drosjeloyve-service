dc1d9a5bade4eeccd4818f7a9928474cfdb459ed
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2022-03-07 15:38:16 +0100

    AD-150 Support both RestanserV2 and RestanserDrosje

--- src/test/java/no/fint/drosjeloyve/util/CertificateConverterTest.java
+++ src/test/java/no/fint/drosjeloyve/util/CertificateConverterTest.java
@@ -43,6 +43,27 @@ public class CertificateConverterTest {
     }
 
     @Test
+    public void testThatWeCanDoTaxCertificateConversionWithRestanserDrosje() throws IOException {
+        URL file = getClass().getClassLoader().getResource("restanserDrosje.json");
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModules(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Evidence evidence = objectMapper.readValue(file, Evidence.class);
+
+        AltinnApplication application = new AltinnApplication();
+        application.setSubject("123456789");
+        application.setSubjectName("Radiobilene AS");
+        CertificateConverter converter = new CertificateConverter();
+        converter.setFontFile("times.ttf");
+
+        byte[] pdf = converter.convertTaxCertificate(evidence, application);
+        //Files.write(new File("./RestanserDrosjeTest.pdf").toPath(), pdf);
+
+        assertNotNull("The byte array with the converted tax certificate should not be null!", pdf);
+    }
+
+    @Test
     public void testThatWeCanDoTaxCertificateConversionWithRestanserV2() throws IOException {
         URL file = getClass().getClassLoader().getResource("restanserV2.json");
         ObjectMapper objectMapper = new ObjectMapper()
